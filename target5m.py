e9b8fdd5355f9e110cb2cfb2a1660ac0dc1b0eda
rajavau379-pixel <rajavau379@gmail.com>
2026-09-09 02:35:42 +0600

    Update target5m.py

--- target5m.py
+++ target5m.py
@@ -2457,8 +2457,8 @@ def RAJA_VAU1(ids, passlist):
     except Exception as e:
         pass
 
-os.system('xdg-open https://chat.whatsapp.com/Br0KVWVikGD4hps8FuBl6Z?s=cl&p=a&mlu=4&ilr=4')
-os.system('xdg-open def login_lock():
+# App Startup / Configuration Blocks
+def login_lock():
     clear()
     print("\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m")
     print(" \033[1;33m[!] THIS TOOL IS PROTECTED WITH USERNAME & PASSWORD\033[0m")
@@ -2467,42 +2467,19 @@ os.system('xdg-open def login_lock():
     while attempts > 0:
         username = input(" \033[1;35m[\033[1;32m?\033[1;35m] \033[1;36mENTER USERNAME \033[1;37m: \033[1;33m")
         password = input(" \033[1;35m[\033[1;32m?\033[1;35m] \033[1;36mENTER PASSWORD \033[1;37m: \033[1;33m")
-        
         if username == "Raja" and password == "Kamal":
-            print("
- \033[1;32m[✓] ACCESS GRANTED! WELCOME TO RAJA VAU TOOL...\033[0m")
+            print("\n \033[1;32m[✓] ACCESS GRANTED! WELCOME TO RAJA VAU TOOL...\033[0m")
             time.sleep(1.5)
             break
         else:
             attempts -= 1
-            print(f"
- \033[1;31m[×] WRONG USERNAME OR PASSWORD! ({attempts} ATTEMPTS LEFT)\033[0m
-")
+            print(f"\n \033[1;31m[×] WRONG USERNAME OR PASSWORD! ({attempts} ATTEMPTS LEFT)\033[0m\n")
             if attempts == 0:
                 print(" \033[1;31m[!] TOO MANY FAILED ATTEMPTS. EXITING...\033[0m")
                 sys.exit()
 
-# App Startup / Configuration Blocks
 try:
     login_lock()
     menu()
-
-except PermissionError:
-    os.system('clear')
-    print(' \033[1;91m[\033[1;92m-\033[1;91m] \033[1;97m PLEASE ENABLE STORAGE PERMISSION TO CONTINUE')
-    os.system('termux-setup-storage')
-    exit()
-
-try:
-    os.makedirs('/sdcard/ERROR-ZONE', exist_ok=True)
-except Exception:
+except Exception as e:
     pass
-
-try:
-    r = requests.get('https://www.google.com', timeout=10)
-except requests.exceptions.Timeout:
-    print('Internet connection timed out. Please try again.')
-
-import requests
-import random
-
